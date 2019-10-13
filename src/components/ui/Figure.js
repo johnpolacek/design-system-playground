554d3164caf63e5286f65560d3c7cdb3c4840477
johnpolacek <johnpolacek@gmail.com>
2019-10-13 10:29:53 -0500

    add links to figure img

--- src/components/ui/Figure.js
+++ src/components/ui/Figure.js
@@ -5,16 +5,18 @@ import React from "react"
 
 export default props => (
   <figure sx={{ m: 0, textAlign: "center", pb: 4 }} {...props}>
-    <img
-      sx={{
-        width: "100%",
-        mb: 1,
-        border: "1px solid",
-        borderColor: "rgba(122,122,122,.2)",
-      }}
-      src={props.img}
-      alt={props.alt}
-    />
+    <a href={props.href}>
+      <img
+        sx={{
+          width: "100%",
+          mb: 1,
+          border: "1px solid",
+          borderColor: "rgba(122,122,122,.2)",
+        }}
+        src={props.img}
+        alt={props.alt}
+      />
+    </a>
     <figcaption>{props.children}</figcaption>
   </figure>
 )
